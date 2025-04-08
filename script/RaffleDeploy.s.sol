bacbb6e720e9508fd86248d33a5d75e5ff7becfe
etheriannn <rianziwalker@gmail.com>
2025-04-08 11:24:18 +0700

    feat: add RaffleDeploy script for deploying Raffle contract with HelperConfig integration

---
+++ script/RaffleDeploy.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract RaffleDeploy is Script {
+    function run() public {}
+
+    function raffleDeploy() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        // local -> deploy mock, get local config
+        // sepolia -> get sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.callbackGasLimit,
+            config.subscriptionId
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
+}

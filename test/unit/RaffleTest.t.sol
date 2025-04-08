8d6d2ebc562d062d565f9acab435d2e76ad59b15
etheriannn <rianziwalker@gmail.com>
2025-04-08 11:24:24 +0700

    feat: add RaffleTest contract for unit testing Raffle functionality

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {RaffleDeploy} from "script/RaffleDeploy.s.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract RaffleTest is Test {}

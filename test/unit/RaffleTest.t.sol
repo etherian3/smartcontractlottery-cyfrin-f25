5837defd0c81a1c8dc3766393cb85b2d57d292bf
etheriannn <rianziwalker@gmail.com>
2025-04-08 14:05:12 +0700

    feat: expand RaffleTest contract with setup and initial state test

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -4,5 +4,35 @@ pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {RaffleDeploy} from "script/RaffleDeploy.s.sol";
 import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
 
-contract RaffleTest is Test {}
+contract RaffleTest is Test {
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+
+    uint256 entranceFee;
+    uint256 interval;
+    address vrfCoordinator;
+    bytes32 gasLane;
+    uint32 callbackGasLimit;
+    uint256 subscriptionId;
+
+    address public PLAYER = makeAddr("player");
+    uint256 public constant STARTING_PLAYER_BALANCE = 10 ether;
+
+    function setUp() external {
+        RaffleDeploy raffleDeploy = new RaffleDeploy();
+        (raffle, helperConfig) = raffleDeploy.raffleDeploy();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        entranceFee = config.entranceFee;
+        interval = config.interval;
+        vrfCoordinator = config.vrfCoordinator;
+        gasLane = config.gasLane;
+        callbackGasLimit = config.callbackGasLimit;
+        subscriptionId = config.subscriptionId;
+    }
+
+    function testRaffleInitializesInOpenState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+}

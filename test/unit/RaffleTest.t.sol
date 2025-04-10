8f31f9c7de89cb1a48ef83a2039f17024feb7fa0
etheriannn <rianziwalker@gmail.com>
2025-04-10 21:43:45 +0700

    feat: add checkUpkeep tests to validate upkeep conditions in Raffle

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -82,4 +82,38 @@ contract RaffleTest is Test {
         vm.prank(PLAYER);
         raffle.enterRaffle{value: entranceFee}();
     }
+
+
+                    /// CHECKUPKEEP //
+                    
+
+    function testCheckupKeepReturnsFalseIfPeopleHasNoBalance() public {
+        // Arrange 
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        // Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(!upkeepNeeded);
+    }
+
+
+    function testCheckUpkeepReturnsFalseIfRaffleIsntOpen() public {
+        // Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        // Act 
+        (bool upkeepNeeded,) = raffle.checkUpkeep("");
+
+        // Assert
+        assert(!upkeepNeeded);
+    }
+
+
 }

8fb1a5652f05b82ff7a244435cafe884fae015e4
etheriannn <rianziwalker@gmail.com>
2025-04-10 16:51:34 +0700

    feat: update subscriptionId for Sepolia VRF in HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -59,7 +59,7 @@ contract HelperConfig is CodeConstants, Script {
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000, // 500,000 gas
-                subscriptionId: 0,
+                subscriptionId: 109546626031412874281493479569127174259968758167565616546757872330979258561879, // subscriptionId for Sepolia VRF
                 link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
             });
     }
